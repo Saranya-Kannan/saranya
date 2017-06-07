eb8a5779698c196b2850959a4d35f27195470f46
Saranya-Kannan <saranhari1997@gmail.com>
2017-06-07 16:00:15 +0530

    Create pnz.java

---
+++ pnz.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class pnz
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+{
+if(n>0)
+{
+System.out.println("postive");
+}
+else if(n<0)
+{
+System.out.println("negative");
+}
+else if(n==0)
+{
+System.out.println("zero");
+}
+}
+}
+}
